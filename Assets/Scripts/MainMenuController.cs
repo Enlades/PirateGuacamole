1c8f1add3fa443027c32eba47aec774a7e084c56
Enlades <muratttkaan@gmail.com>
2018-01-21 22:28:43 +0300

    - Main menu added. - SoundSFX added to assets.

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+    public AudioSource SfxSource;
+
+    public AudioClip ButtonSfx;
+
+    public void PlayButtonSound() {
+        SfxSource.clip = ButtonSfx;
+        SfxSource.Play();
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
